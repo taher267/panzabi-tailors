7c1a78788850ec3e1ba24fb7f9adc29321febff9
ABU TAHER <abutaher267@gmail.com>
2022-12-12 00:53:34 +0600

    product

--- src/component/Admin/Order/OrderProduct.jsx
+++ src/component/Admin/Order/OrderProduct.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Autocomplete, TextField } from '@mui/material';
+import { Autocomplete, Stack, TextField } from '@mui/material';
 
 export default function OrderProduct({
   products,
@@ -9,27 +9,44 @@ export default function OrderProduct({
 }) {
   // const options = [{ name: 'The Shawshank Redemption', y: 1994 }];
   return (
-    <Autocomplete
-      multiple
-      id="multiple-limit-tags"
-      options={products}
-      getOptionLabel={(o) => o.name}
-      filterSelectedOptions
-      onChange={selectedProducts}
-      defaultValue={defaultProducts}
-      renderInput={(p) => {
-        return (
+    <Stack spacing={3} sx={{ width: 500 }}>
+      <Autocomplete
+        multiple
+        id="tags-standard"
+        options={products}
+        getOptionLabel={(option) => option.name}
+        defaultValue={defaultProducts}
+        renderInput={(params) => (
           <TextField
-            helperText={error?.message || ''}
-            {...p}
-            error={error ? true : false}
-            label="Products"
-            variant="filled"
+            {...params}
+            variant="standard"
+            label="Multiple values"
             placeholder="Favorites"
           />
-        );
-      }}
-      sx={{ width: '500px' }}
-    />
+        )}
+      />
+      {/* <Autocomplete
+        multiple
+        id="tags-standard"
+        options={products}
+        getOptionLabel={(o) => o.name}
+        filterSelectedOptions
+        onChange={selectedProducts}
+        defaultValue={defaultProducts}
+        renderInput={(p) => {
+          return (
+            <TextField
+              helperText={error?.message || ''}
+              {...p}
+              error={error ? true : false}
+              label="Products"
+              variant="filled"
+              placeholder="Favorites"
+            />
+          );
+        }}
+        sx={{ width: '500px' }}
+      /> */}
+    </Stack>
   );
 }
