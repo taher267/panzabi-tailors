916d2a4fa0624f1754e5cef030a44471f81ae890
ABU TAHER <abutaher267@gmail.com>
2022-12-28 01:09:49 +0600

    url change

--- client/src/apolloClient.jsx
+++ client/src/apolloClient.jsx
@@ -1,11 +1,15 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { ReactSession } from 'react-client-session';
+// console.log(import.meta.env.MODE);
 const httpLink = createHttpLink({
   // uri: 'https://precious-daffodil-da626b.netlify.app',
   // uri: 'https://6339dccf0558fc1390238f4c--precious-daffodil-da626b.netlify.app',
-  // uri: 'https://panzabi-tailors.onrender.com',
-  uri: 'http://localhost:4000',
+  uri:
+    import.meta.env.MODE === 'production'
+      ? 'https://panzabi-tailors.onrender.com'
+      : 'http://localhost:4000',
+  // uri: 'http://localhost:4000',
 });
 const authLink = setContext((_, { headers }) => {
   let token = ReactSession.get('token');
