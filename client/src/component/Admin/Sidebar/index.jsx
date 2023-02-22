9b9bd2d62f9b2e29ef9d11a7d2e980912833b10d
ABU TAHER <abutaher267@gmail.com>
2023-02-23 03:05:36 +0600

    client sidebar

--- client/src/component/Admin/Sidebar/index.jsx
+++ client/src/component/Admin/Sidebar/index.jsx
@@ -1,52 +1,48 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Typography from '@mui/material/Typography';
+import ContentCut from '@mui/icons-material/ContentCut';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import Cloud from '@mui/icons-material/Cloud';
 import { Link } from 'react-router-dom';
-
-const Sidebar = ({ position }) => {
+const menuItems = [
+  { url: '/dashboard/order', name: 'অর্ডার সমুহঃ' },
+  // { url: '/dashboard/order/new', name: 'নতুন অর্ডারঃ' },
+  { url: '/dashboard/design', name: 'নকশাসমূহ' },
+  { url: '/dashboard/design/new', name: 'নতুন নকশা' },
+  { url: '/dashboard/customer', name: 'গ্রাহকসমূহ' },
+  { url: '/dashboard/customer/new', name: 'নতুন গ্রাহক' },
+  { url: '/dashboard/product', name: 'পণ্যসমূহ' },
+  { url: '/dashboard/product/new', name: 'নতুন পণ্য' },
+  { url: '/dashboard/measurement', name: 'পরিমাপ' },
+  { url: '/dashboard/measurement/new', name: 'নতুন পরিমাপ' },
+  { url: '/template', name: 'Template' },
+  { url: '/template/new', name: 'New Template' },
+  { url: '/dashboard/account', name: 'হিসাবসমূহ' },
+  { url: '/dashboard/account/new', name: 'নতুন হিসাব' },
+];
+export default function Sidebar() {
   return (
-    <ul style={{ listStyle: 'none', position }}>
-      <li>
-        <Link to="/dashboard/order">অর্ডার সমুহঃ</Link>
-      </li>
-      {/* <li>
-        <Link to="/dashboard/order/new">নতুন অর্ডারঃ</Link>
-      </li> */}
-      <li>
-        <Link to="/dashboard/design">নকশাসমূহ</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/design/new">নতুন নকশা</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/customer">গ্রাহকসমূহ</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/customer/new">নতুন গ্রাহক</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/product">পণ্যসমূহ</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/product/new">নতুন পণ্য</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/measurement">পরিমাপ</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/measurement/new">নতুন পরিমাপ</Link>
-      </li>
-      <li>
-        <Link to="/template">Template</Link>
-      </li>
-      <li>
-        <Link to="/template/new">New Template</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/account">হিসাবসমূহ</Link>
-      </li>
-      <li>
-        <Link to="/dashboard/account/new">নতুন হিসাব</Link>
-      </li>
-    </ul>
+    <Paper sx={{ width: '100%', maxWidth: '100%' }}>
+      <MenuList>
+        {menuItems?.map?.(({ url, name }) => (
+          <MenuItem key={name}>
+            <ListItemIcon>⌘</ListItemIcon>
+            <ListItemText>
+              <Link to={url}>{name}</Link>
+            </ListItemText>
+            <Typography variant="body2" color="text.secondary">
+              ⌘
+            </Typography>
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Paper>
   );
-};
-
-export default Sidebar;
+}
--- client/src/component/Layout/AdminLayout/index.jsx
+++ client/src/component/Layout/AdminLayout/index.jsx
@@ -1,43 +1,121 @@
 import Box from '@mui/material/Box';
 import Sidebar from '../../Admin/Sidebar';
 import Head from '../../graphql/Head';
-import AllOutIcon from '@mui/icons-material/AllOut';
+// import AllOutIcon from '@mui/icons-material/AllOut';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import React from 'react';
+import { Typography } from '@mui/material';
+// import useMediaQuery from '@mui/material/useMediaQuery';
 
+let i = 20;
 const AdminLayout = ({ children, title, rightSX, leftSX }) => {
-  const [width, setWidth] = React.useState({ l: 20, r: 80 });
+  const [width, setWidth] = React.useState(20);
   const [isExpend, setisExpend] = React.useState(false);
   const widthHandler = () => {
-    setisExpend((p) => !p);
-    setWidth((p) => (p.l === 20 ? { l: 0, r: 100 } : { l: 20, r: 80 }));
+    let clrInterval;
+    if (width > 0) {
+      clrInterval = setInterval(() => {
+        if (i < 0) return clearInterval(clrInterval);
+        setWidth((p) => {
+          const k = i--;
+          return k;
+        });
+      }, 0);
+    } else {
+      clrInterval = setInterval(() => {
+        if (i > 19) return clearInterval(clrInterval);
+        setWidth((p) => {
+          const k = i++;
+          return k;
+        });
+      }, 0);
+    }
+
+    // setisExpend((p) => {
+    //   let incDec = 20;
+    //   if (incDec !== 0) {
+    //     for (let i = 20; i < 20; i--) {
+    //       console.log(i);
+    //       setWidth(i);
+    //     }
+    //   }
+    //   return !p;
+    // });
+    // setWidth((p) => (p.l === 20 ? { l: 0, r: 100 } : { l: 20, r: 80 }));
   };
+  // const match = useMediaQuery('(max-width:900px)');
+  // console.log(match);
   return (
     <Box
       sx={{
         width: '100%',
         height: '100%',
         display: 'flex',
-        marginTop: 3,
+        marginTop: { md: 3 },
         position: 'relative',
       }}
     >
       <Head title={title} />
-      <AllOutIcon
+      <Typography
         onClick={widthHandler}
-        sx={{ position: 'absolute', marginLeft: '19%', zIndex: 99999 }}
-      />
+        sx={{
+          border: '1px solid #ddd',
+          color: '#009dea',
+          position: 'absolute',
+          marginLeft: {
+            xs: `${(20 - i) * 10 > 200 ? 175 : (20 - i) * 10}px`,
+            sm: `${(20 - i) * 10 > 200 ? 175 : (20 - i) * 10}px`,
+            md: `${i < 2 ? 0 : i - 1.5}%`,
+          },
+          // marginLeft: isExpend ? '' : `${i < 0 ? 0 : i}%`,
+          zIndex: 100,
+          cursor: 'pointer',
+        }}
+      >
+        {i < 2 ? <OpenInFullIcon /> : <CloseFullscreenIcon />}
+      </Typography>
+
       <Box
         sx={{
-          width: { xs: 0, sm: 0, md: `${width?.l || ''}%` },
-          paddingX: { xs: 0, sm: 0, md: 2 },
-          display: {
-            xs: isExpend ? 'inline-block' : 'none',
-            sm: isExpend ? 'inline-block' : 'none',
-            md: isExpend ? 'inline-block' : 'none',
-            // xs: width?.l ? 'inline-block' : 'none',
-            // sm: width?.l ? 'inline-block' : 'none',
-            // md: width?.l ? 'inline-block' : 'none',
+          width: {
+            xs: i < 2 ? '200px' : 0,
+            sm: i < 2 ? '200px' : 0,
+            md: `${i}%`,
+            lg: `${i}%`,
+
+            // md: isExpend ? 0 : `${width}%`,
+            // lg: isExpend ? 0 : `${width}%`,
           },
+          background: {
+            xs: '#ddd',
+            sm: '#ddd',
+            md: 'transparent',
+          },
+          paddingX: { xs: 0, sm: 0, md: 2 },
+          paddingY: { xs: 2, sm: 2, md: 0 },
+          marginTop: { xs: 0, sm: 0 },
+          display: {
+            xs: i < 2 ? 'inline-block' : 'none',
+            sm: i < 2 ? 'inline-block' : 'none',
+            md: i < 2 ? 'none' : 'inline-block',
+            lg: i < 2 ? 'none' : 'inline-block',
+            // xs: isExpend ? 'inline-block' : 'none',
+            // sm: isExpend ? 'inline-block' : 'none',
+            // md: isExpend ? 'none' : 'inline-block',
+            // lg: isExpend ? 'none' : 'inline-block',
+          },
+          // position: { xs: 'absolute', sm: 'inherit' },
+          position: {
+            xs: i < 2 ? 'absolute' : '',
+            sm: i < 2 ? 'absolute' : '',
+            md: i < 2 ? 'absolute' : '',
+            // xs: isExpend ? 'absolute' : '',
+            // sm: isExpend ? 'absolute' : '',
+            // md: isExpend ? 'absolute' : '',
+          },
+          zIndex: { xs: i < 2 ? 99 : '' },
+          // zIndex: { xs: isExpend ? 99 : '' },
           height: '100%',
           ...leftSX,
         }}
@@ -46,7 +124,7 @@ const AdminLayout = ({ children, title, rightSX, leftSX }) => {
       </Box>
       <Box
         sx={{
-          width: { xs: '100%', sm: '100%', md: `${width?.r || ''}%` },
+          width: { xs: '100%', sm: '100%', md: `${isExpend ? 100 : 100 - i}%` },
           display: { xs: 'block', sm: 'block', md: 'inline-block' },
           padding: { xs: 2, sm: 1.5 },
           height: '100%',
